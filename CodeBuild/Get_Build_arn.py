3d9dcfb44d2a629742ba05897a010dd133c07de1
Navidehbaghaeifar <navidehbaghaifar@gmail.com>
2023-07-05 18:48:29 +0000

    get build arn

---
+++ CodeBuild/Get_Build_arn.py
@@ -0,0 +1,12 @@
+import boto3
+
+codebuild = boto3.client('codebuild')
+
+response = codebuild.batch_get_builds(
+    ids=[
+        'developer-labs:9762b47f-970f-4d20-8155-bebe3d74f562',
+    ]
+)
+builds = response["builds"]
+for build in builds:
+    print(build["id"],build["arn"])
